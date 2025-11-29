db92f3a9195020c2815df8df005313a16c8cba28
Bradley Lyman <lyman.brad3211@gmail.com>
2025-11-29 13:33:26 -0700

    replace old implementation with demo_vk version

--- src/graphics/vulkan_api/texture.rs
+++
@@ -1,8 +0,0 @@
-use crate::graphics::vulkan_api::raii;
-
-/// Represents a 2D rgba texture which can be used by shaders.
-#[derive(Debug)]
-pub struct Texture2D {
-    pub image_view: raii::ImageView,
-    pub image: raii::Image,
-}
---
+++ src/main.rs
@@ -0,0 +1,130 @@
+use {
+    anyhow::Result,
+    ash::vk,
+    clap::Parser,
+    demo_vk::{
+        demo::{Demo, Graphics, demo_main},
+        graphics::vulkan::Frame,
+    },
+    glfw::Window,
+};
+
+#[derive(Debug, Parser)]
+struct Args {}
+
+type Gfx = Graphics<Args>;
+
+struct Example {}
+
+impl Demo for Example {
+    type Args = Args;
+
+    /// Specify physical device features if anything non-default is required
+    fn physical_device_dynamic_rendering_features()
+    -> vk::PhysicalDeviceDynamicRenderingFeatures<'static> {
+        vk::PhysicalDeviceDynamicRenderingFeatures {
+            dynamic_rendering: vk::TRUE,
+            ..Default::default()
+        }
+    }
+
+    /// Initialize the demo
+    fn new(_window: &mut Window, _gfx: &mut Gfx) -> Result<Self> {
+        Ok(Self {})
+    }
+
+    /// Draw a frame
+    fn draw(&mut self, _window: &mut Window, gfx: &mut Gfx, frame: &Frame) -> Result<()> {
+        let image_memory_barrier = vk::ImageMemoryBarrier {
+            old_layout: vk::ImageLayout::UNDEFINED,
+            new_layout: vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL,
+            src_access_mask: vk::AccessFlags::empty(),
+            dst_access_mask: vk::AccessFlags::COLOR_ATTACHMENT_WRITE,
+            image: frame.swapchain_image(),
+            subresource_range: vk::ImageSubresourceRange {
+                aspect_mask: vk::ImageAspectFlags::COLOR,
+                base_mip_level: 0,
+                level_count: 1,
+                base_array_layer: 0,
+                layer_count: 1,
+            },
+            ..Default::default()
+        };
+        unsafe {
+            gfx.vulkan.cmd_pipeline_barrier(
+                frame.command_buffer(),
+                vk::PipelineStageFlags::TOP_OF_PIPE
+                    | vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+                vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+                vk::DependencyFlags::empty(),
+                &[],
+                &[],
+                &[image_memory_barrier],
+            );
+        }
+
+        unsafe {
+            let color_attachments = [vk::RenderingAttachmentInfo {
+                image_view: frame.swapchain_image_view(),
+                image_layout: vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL,
+                resolve_mode: vk::ResolveModeFlags::NONE,
+                load_op: vk::AttachmentLoadOp::CLEAR,
+                store_op: vk::AttachmentStoreOp::STORE,
+                clear_value: vk::ClearValue {
+                    color: vk::ClearColorValue {
+                        float32: [0.0, 0.0, 0.0, 0.0],
+                    },
+                },
+                ..Default::default()
+            }];
+            gfx.vulkan.cmd_begin_rendering(
+                frame.command_buffer(),
+                &vk::RenderingInfo {
+                    render_area: vk::Rect2D {
+                        offset: vk::Offset2D { x: 0, y: 0 },
+                        extent: gfx.swapchain.extent(),
+                    },
+                    layer_count: 1,
+                    color_attachment_count: 1,
+                    p_color_attachments: color_attachments.as_ptr(),
+                    ..Default::default()
+                },
+            );
+
+            gfx.vulkan.cmd_end_rendering(frame.command_buffer());
+        }
+
+        let image_memory_barrier = vk::ImageMemoryBarrier {
+            old_layout: vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL,
+            new_layout: vk::ImageLayout::PRESENT_SRC_KHR,
+            src_access_mask: vk::AccessFlags::COLOR_ATTACHMENT_WRITE,
+            dst_access_mask: vk::AccessFlags::empty(),
+            image: frame.swapchain_image(),
+            subresource_range: vk::ImageSubresourceRange {
+                aspect_mask: vk::ImageAspectFlags::COLOR,
+                base_mip_level: 0,
+                level_count: 1,
+                base_array_layer: 0,
+                layer_count: 1,
+            },
+            ..Default::default()
+        };
+        unsafe {
+            gfx.vulkan.cmd_pipeline_barrier(
+                frame.command_buffer(),
+                vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+                vk::PipelineStageFlags::BOTTOM_OF_PIPE,
+                vk::DependencyFlags::empty(),
+                &[],
+                &[],
+                &[image_memory_barrier],
+            );
+        }
+
+        Ok(())
+    }
+}
+
+fn main() {
+    demo_main::<Example>();
+}
